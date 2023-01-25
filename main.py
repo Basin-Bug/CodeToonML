0619279924bcc190f739e5975f0ea350b9c611cd
Basin-Bug <71539948+Basin-Bug@users.noreply.github.com>
2023-01-26 00:14:06 +0900

    Master

--- main.py
+++ main.py
@@ -19,6 +19,8 @@ episode_num = []
 running_new = []
 max_reward = 0
 
+
+
 def solve_input(game, inp):
 
   if inp == "rewards":
@@ -62,36 +64,37 @@ def process_action(game, action):
   j = (action % 50) % 5
   if choice == 0:
     if i <= 4:
-      print("attack(enemy.memory[{}][{}]);".format(i, j))
+      get_agent_move = "attack(enemy.memory[{}][{}]);".format(i, j)
     else:
-      print("attack(memory[{}][{}]);".format(i - 5, j))
+      get_agent_move = "attack(memory[{}][{}]);".format(i - 5, j)
     game.attack(i, j, False)
     
   elif choice == 1:
     r = random.randint(1000, 10000)
     if i <= 4:
-      print("attack(enemy.memory[{}][{}], {});".format(i, j, r))
+      get_agent_move = "attack(enemy.memory[{}][{}], {});".format(i, j, r)
     else:
-      print("attack(memory[{}][{}], {});".format(i - 5, j, r))
+      get_agent_move = "attack(memory[{}][{}], {});".format(i - 5, j, r)
     game.attack2(i, j, r, False)
     
   elif choice == 2:
     r = random.randint(1000, 10000)
     if i <= 4:
-      print("enemy.memory[{}][{}].setPass({});".format(i, j, r))
+      get_agent_move = "enemy.memory[{}][{}].setPass({});".format(i, j, r)
     else:
-      print("memory[{}][{}].setPass({})".format(i - 5, j, r))
+      get_agent_move = "memory[{}][{}].setPass({})".format(i - 5, j, r)
     game.lock(i, j, r, False)
     
   elif choice == 3:
     bef = game.is_locked[5 * i + j]
     r = random.randint(1000, 10000)
     if i <= 4:
-      print("enemy.memory[{}][{}].setPass({}, {});".format(i, j, bef, r))
+      get_agent_move = "enemy.memory[{}][{}].setPass({}, {});".format(i, j, bef, r)
     else:
-      print("memory[{}][{}].setPass({}, {})".format(i - 5, j , bef, r))
+      get_agent_move = "memory[{}][{}].setPass({}, {})".format(i - 5, j , bef, r)
     game.lock2(i, j, bef, r, False)
     
+  return get_agent_move
 
 
 def get_input():
@@ -101,10 +104,11 @@ def get_input():
 def agent_move():
   aprob = pg.forward(game.reward)
   action = pg.select_action(aprob)
-  process_action(game, action)
+  agent_mv = process_action(game, action)
   reward = game.calc_reward()
   time.sleep(5)
   
+  return agent_mv
 while 1:
   if game.calc_reward == 50 or game.calc_reward == -50:
     print("#####GAME OVER#####")
@@ -113,10 +117,7 @@ while 1:
     max_reward = max(max_reward, game.calc_reward)
   if episode_number == 2000:
     break
-  """if __name__ == '__main__':
-    with ProcessPoolExecutor(max_workers=2) as executor:
-      executor.submit(get_input)
-      executor.submit(agent_move)"""
   get_input()
-  agent_move()  
+  move = agent_move()
+  print(move)  
   print("agent's reward: {}".format(game.calc_reward()))
